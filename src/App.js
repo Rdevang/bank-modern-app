05b9ce99f90cc9cec78b9d61b3fa64c6613d2033
Rdevang <rdevang170@gmail.com>
2024-01-08 20:00:37 +0530

    final

--- src/App.js
+++ src/App.js
@@ -39,6 +39,27 @@ function App() {
           <Footer />
         </div>
       </div>
+      <button
+        
+        class="py-4 hover:scale-125 fixed px-6 bg-blue-gradient font-poppins font-bold bottom-[10%] right-10 text-[18px] text-primary outline-none rounded-lg z-50"
+        data-projection-id="8"
+      >
+        
+        <a href="#home"> <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-3 h-3 md:h-6 md:w-6 "
+
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.47 2.47a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06l-6.22-6.22V21a.75.75 0 01-1.5 0V4.81l-6.22 6.22a.75.75 0 11-1.06-1.06l7.5-7.5z"
+            clip-rule="evenodd"
+          ></path>
+        </svg></a>
+       
+      </button>
     </div>
   );
 }
--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,12 +4,12 @@ import styles from '../styles';
 
 const CTA = () => {
   return (
-    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flow-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
       <div className="flex flex-col flex-1">
         <h2 className={`${styles.heading2}`}>Manage your finance with us!</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
-      <div className={`${styles.flexCenter}`}>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
         <Buttons/>
       </div>
     </section>
